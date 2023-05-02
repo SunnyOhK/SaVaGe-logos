fcb5a6173f326598631de9998ba0e79fa1c2c737
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-05-02 17:48:26 -0500

    working through passing tests due to spacing

--- extra-code-JIC/savedJIC.js
+++
@@ -1,6 +0,0 @@
-// Import shape classes from shapes.js and define each shape
-const { Circle, Square, Triangle, Diamond } = require('./lib/shapes.js');
-const circle = new Circle();
-const square = new Square();
-const triangle = new Triangle();
-const diamond = new Diamond();
